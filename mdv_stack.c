bee5123e94a0b562de9e4e5bb3c30b2b38bcafba
Mdv17 <112490102+Mdv17@users.noreply.github.com>
2024-01-22 09:22:58 +0200

    Update mdv_stack.c

--- mdv_stack.c
+++ mdv_stack.c
@@ -6,10 +6,9 @@
  * @line_number: line number
  * Return: 0 on success, EXIT_FAILURE on failure
  */
-int mdv_stack(stack_t **head, unsigned int line_number)
+void mdv_stack(stack_t **head, unsigned int line_number)
 {
     (void)head; /* Unused parameter */
     (void)line_number; /* Unused parameter */
     bus.lifi = 0; /* Set the stack mode to stack */
-    return 0; /* Return success */
 }
