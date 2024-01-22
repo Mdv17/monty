e85aa7dbb4137f23addfae5fbe6752ed7e6bf560
Mdv17 <112490102+Mdv17@users.noreply.github.com>
2024-01-22 09:20:25 +0200

    Update mdv_rotr.c

--- mdv_rotr.c
+++ mdv_rotr.c
@@ -1,33 +1,26 @@
 #include "monty.h"
-
 /**
- * mdv_rotr - rotates the stack to the bottom
- * @head: stack head
- * @line_number: line number
- * Return: 0 on success, EXIT_FAILURE on failure
+  *mdv_rotr- rotates the stack to the bottom
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
  */
-int mdv_rotr(stack_t **head, unsigned int line_number)
+void mdv_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
-    stack_t *last; /* Pointer to the last element of the stack */
+	stack_t *copy;
 
-    (void)line_number; /* Unused parameter */
-    last = *head;
-    /* Check if the stack is empty or has only one element */
-    if (*head == NULL || (*head)->next == NULL)
-    {
-        return 0; /* Return success */
-    }
-    /* Loop until the end of the stack */
-    while (last->next)
-    {
-        last = last->next;
-    }
-    /* Move the last element to the top of the stack */
-    last->next = *head;
-    last->prev->next = NULL;
-    last->prev = NULL;
-    (*head)->prev = last;
-    (*head) = last;
-    /* Return success */
-    return 0;
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
 }
