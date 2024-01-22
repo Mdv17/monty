07108703d624a225fdf13af1aeb1527d64297348
Mdv17 <112490102+Mdv17@users.noreply.github.com>
2024-01-22 08:43:30 +0200

    Update mdv_pop.c

--- mdv_pop.c
+++ mdv_pop.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * f_pop - prints the top
+ * mdv_pop - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_pop(stack_t **head, unsigned int counter)
+void mdv_pop(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 
