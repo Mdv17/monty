7e2147498b1f14902f35aa887c60e097829d3d40
Mdv17 <112490102+Mdv17@users.noreply.github.com>
2024-01-22 08:44:21 +0200

    Update mdv_pstr.c

--- mdv_pstr.c
+++ mdv_pstr.c
@@ -1,30 +1,25 @@
 #include "monty.h"
-
 /**
- * mdv_pstr - prints the string starting at the top of the stack,
- * followed by a new line
+ * f_pstr - prints the string starting at the top of the stack,
+ * followed by a new
  * @head: stack head
- * @line_number: line number
- * Return: 0 on success, EXIT_FAILURE on failure
- */
-int mdv_pstr(stack_t **head, unsigned int line_number)
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pstr(stack_t **head, unsigned int counter)
 {
-    stack_t *top; /* Pointer to the top of the stack */
+	stack_t *h;
+	(void)counter;
 
-    (void)line_number; // Unused parameter
-    top = *head;
-    /* Loop through the stack and print each character until a non-printable one is found */
-    while (top)
-    {
-        if (top->n > 127 || top->n <= 0)
-        {
-            break;
-        }
-        printf("%c", top->n);
-        top = top->next;
-    }
-    printf("\n");
-    /* Return success */
-    return 0;
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
 }
-
